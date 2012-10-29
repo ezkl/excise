ed11c6ba2ddc00c0ca34f1012c6386790fd9e1a7
Ezekiel Templin <zeke@templ.in>
2012-10-29 09:16:14 -0400

    Add `#parse_string` and make string r/w

--- lib/excise/base.rb
+++ lib/excise/base.rb
@@ -5,6 +5,7 @@ module Excise
     TOKEN_EXPRESSION = /{([^{}\t\r\n]+)}/.freeze
     SPECIAL_CHARACTERS_EXPRESSION = /[\\\^\$\*\+\.\?\(\)\[\]]/.freeze
 
+    attr_accessor :string
 
     def initialize(pattern, string = "")
       @pattern = pattern
@@ -12,15 +13,20 @@ module Excise
       @tokens = tokens
       @pattern_expression = pattern_expression
 
-      @matches = @pattern_expression.match(@string)
       @output = {}
     end
 
     def parse
+      @matches = @pattern_expression.match(@string)
       @tokens.each_with_index do |key, index|
         @output[key.to_sym] = @matches[index+1]
       end
       @output
+    end
+
+    def parse_string(string)
+      @string = string
+      parse
     end
 
     def tokens
--- spec/excise/base_spec.rb
+++ spec/excise/base_spec.rb
@@ -3,8 +3,13 @@ require "spec_helper"
 module Excise
   describe "Base" do
     context 'only a pattern' do
-      subject { Base.new(pattern) }
+      let(:base) { Base.new(pattern) }
       let(:pattern) { '[{first}]' }
+
+      it "should parse strings" do
+        base.string = '[value]'
+        base.parse.should be_a Hash
+      end
     end
 
     context 'pattern and string' do
@@ -16,6 +21,17 @@ module Excise
       it           { should be_a Hash }
       its(:keys)   { should eq [:first, :second, :third] }
       its(:values) { should eq ['this', 'patterned', 'STRING'] }
+    end
+
+    describe '#parse_string' do
+      let(:pattern) { Base.new('[{first}]') }
+      it "should take a string and parse it" do
+        output = pattern.parse_string('[value]')
+        output.should eq Hash[{first: 'value'}]
+
+        output = pattern.parse_string('[other]')
+        output.should eq Hash[{first: 'other'}]
+      end
     end
   end
 end
