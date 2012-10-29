b86b19677ae8499f44867511ef20378628a7ab1d
Ezekiel Templin <zeke@templ.in>
2012-10-29 09:02:23 -0400

    Memoize patterns

--- lib/excise/base.rb
+++ lib/excise/base.rb
@@ -2,34 +2,41 @@
 
 module Excise
   class Base
-    def initialize(string, pattern)
-      @string  = string
+    TOKEN_EXPRESSION = /{([^{}\t\r\n]+)}/.freeze
+    SPECIAL_CHARACTERS_EXPRESSION = /[\\\^\$\*\+\.\?\(\)\[\]]/.freeze
+
+
+    def initialize(pattern, string = "")
       @pattern = pattern
-      @matches = pattern_expression.match(@string)
+      @string = string
+      @tokens = tokens
+      @pattern_expression = pattern_expression
+
+      @matches = @pattern_expression.match(@string)
       @output = {}
     end
 
     def parse
-      tokens.each_with_index do |key, index|
+      @tokens.each_with_index do |key, index|
         @output[key.to_sym] = @matches[index+1]
       end
       @output
     end
 
     def tokens
-      @pattern.scan(token_expression).flatten
+      @tokens ||= @pattern.scan(token_expression).flatten
     end
 
     def pattern_expression
-      Regexp.new(@pattern.gsub(special_characters_expression, '\\\\\0').gsub(token_expression, "(\.+)"))
+      @pattern_expression ||= Regexp.new(@pattern.gsub(special_characters_expression, '\\\\\0').gsub(token_expression, "(\.+)"))
     end
 
     def token_expression
-      /{([^{}\t\r\n]+)}/
+      TOKEN_EXPRESSION
     end
 
     def special_characters_expression
-      /[\\\^\$\*\+\.\?\(\)\[\]]/
+      SPECIAL_CHARACTERS_EXPRESSION
     end
   end
 end
--- spec/excise/base_spec.rb
+++ spec/excise/base_spec.rb
@@ -2,8 +2,13 @@ require "spec_helper"
 
 module Excise
   describe "Base" do
-    context 'patterned string' do
-      subject       { Base.new(string, pattern).parse }
+    context 'only a pattern' do
+      subject { Base.new(pattern) }
+      let(:pattern) { '[{first}]' }
+    end
+
+    context 'pattern and string' do
+      subject       { Base.new(pattern, string).parse }
 
       let(:string)  { '[this] (patterned) <STRING>' }
       let(:pattern) { '[{first}] ({second}) <{third}>' }
